8abeba028cff893e9805cd211a680b2b580e08a8
Artem Kheifets <artmarsik@yandex.ru>
2020-06-17 16:09:14 +0300

    base.html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def posts_list(request):
+    n = ['Dasha', 'Moshe', 'Semen']
+    return render(request, 'blog/index.html', context={'names': n})
