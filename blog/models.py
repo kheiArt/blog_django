0e263cc9551afb52e7adc345e49fbfee3711e50e
Artem Kheifets <artmarsik@yandex.ru>
2020-06-17 23:33:09 +0300

    added model Tag

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.shortcuts import reverse
 
 
 class Post(models.Model):
@@ -6,7 +7,18 @@ class Post(models.Model):
     slug = models.SlugField(max_length=150, blank=True, unique=True)
     body = models.TextField(blank=True, db_index=True)
     date_pub = models.DateTimeField(auto_now_add=True)
-    # tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
+    tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
+
+    def __str__(self):
+        return '{}'.format(self.title)
+
+    def get_absolute_url(self):
+        return reverse("post_detail_url", kwargs={"slug": self.slug})
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
 
     def __str__(self):
         return '{}'.format(self.title)
